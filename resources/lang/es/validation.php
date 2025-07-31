68ca4bdb57cba6bf3ddd90436edc148d1d07736a
Reinaldo Duque <reinaldodu@gmail.com>
2025-07-30 19:40:15 -0500

    Actualización traducción validación contraseña

--- resources/lang/es/validation.php
+++ resources/lang/es/validation.php
@@ -4,5 +4,9 @@ return [
 
     'required' => 'El campo :attribute es obligatorio.',
     'unique' => 'El campo :attribute ya existe.',
+    'same' => 'La :attribute no coincide con su confirmación.',
+    'min' => [
+        'string' => 'La :attribute debe contener al menos :min caracteres.',
+    ],
 
 ];
